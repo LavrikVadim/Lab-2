d0957c05cd001ea9a56f63512b670b70477f1457
vadim <lavrik-v05@mail.ru>
2023-11-15 22:17:06 +0300

    finish changes

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from csv import reader
 
 
-# flag = 0
-# output = open('result.txt', 'w')
-# search = input('Search for: ')
 def task1():
     with open('books.csv', 'r', encoding='windows-1251') as csvfile:
         titles = []
@@ -16,6 +13,8 @@ def task1():
             if len(i) > 30:
                 result.append(i)
         print(len(result))
+
+
 
 def task2(): 
     author = input()
@@ -35,28 +34,6 @@ def task2():
             else:
                 print('Таких книг не нашлось. Попробуйте указать другого автора')
        
-task2()       
+      
        
-       
-        #      books.append(row)
-        # books.remove(books[0])
-        # for i in books:
-        #     
-             
-        
-#         lower_case = row[2].lower()
-#         index = lower_case.find(search.lower())
-#         if index != -1:
-#             print(row[2])
-#             flag = 1
-#             output.write(f'{row[0]}. {row[2]}. Цена {row[8]} рублей.\n')
 
-#     if flag == 0:
-#         print('Nothing found.')
-
-# output.close()
-
-# with open('books.csv') as f:
-#     table = reader(f, delimiter=';')
-#     for row in table:
-#         print(row)
